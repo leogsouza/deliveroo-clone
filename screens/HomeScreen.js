59b25d4831c48e5a7caf0b024b4c2fc155c028c9
Leonardo Souza <leogsouza@gmail.com>
2023-03-07 23:11:02 -0300

    [WIP] Configuring home Screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import { View, Text, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const HomeScreen = () => {
+
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
+  return (
+    <SafeAreaView className="my-5">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+              </Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+};
+
+export default HomeScreen;
